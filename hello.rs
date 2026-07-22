497856d0ba4599fb4f7c05f3803b33e0aaa131ac
Karp Foch <karpfoch@proton.me>
2026-07-21 23:02:38 -0400

    Add files via upload

---
+++ hello.rs
@@ -0,0 +1,7 @@
+/* main function is always the first code that runs 
+    in every executable rust program */
+fn main() {
+    println!("hello, world");   // prints text to the screen
+    /* using a ! means that you're calling a macro 
+        instead of a normal function */
+}
